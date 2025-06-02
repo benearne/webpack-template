f044ba41e488e0c178bb91635e8d6c2039c5e773
„benearne“ <b.arnold@posteo.de>
2025-06-02 22:10:40 +0200

    Initial template setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+    clean: true,
+  },
+  mode: 'development',
+  devServer: {
+    static: './dist',
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/template.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
